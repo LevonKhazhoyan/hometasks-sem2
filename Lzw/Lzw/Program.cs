963b5d45df88bc06a43841756045db23af4c4dab
LevonKhazhoyan <levon_hazhoyan@mail.ru>
2022-08-23 15:25:39 +0300

    fixs

---
+++ Lzw/Lzw/Program.cs
@@ -0,0 +1,70 @@
+﻿using Lzw;
+
+Console.WriteLine("Insert \"-c file path\" to compress\n\"-d file path\" to decompress");
+Console.WriteLine($"File path can be global or leads in: {AppDomain.CurrentDomain.BaseDirectory}");
+var input = Console.ReadLine()?.Split(" ");
+try
+{
+    if (input?.Length == 2)
+    {
+        switch (input[0])
+        {
+            case "-c":
+            {
+                if (File.Exists(input[1] + ".zipped"))
+                {
+                    Console.WriteLine(input[1] + ".zipped file already exists. Would you like to delete existing file?\ny/n");
+                    if (Console.ReadLine() == "y")
+                    {
+                        File.Delete(input[1] + ".zipped");
+                    }
+                    else
+                    {
+                        break;
+                    }                    
+                }
+                LzwMethods.Compress(input[1]);
+                var coefficient = (double)new FileInfo(input[1]).Length / new FileInfo(input[1] + ".zipped").Length;
+                Console.WriteLine($"Compression coefficient: {Math.Round(coefficient, 5)}");
+                break;
+            }
+            case "-d":
+            {
+                if (input[1].Length > 7 && input[1][(input[1].Length - 7)..] == ".zipped")
+                {
+                    if (File.Exists(input[1][..(input[1].Length - 7)]))
+                    {
+                        Console.WriteLine(input[1] + ".zipped file already exists. Would you like to delete existing file?\ny/n");
+                        if (Console.ReadLine() == "y")
+                        {
+                            File.Delete(input[1][..(input[1].Length - 7)]);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    LzwMethods.Decompress(input[1]);
+                    Console.WriteLine("File successfully decompressed");
+                    return;
+                }
+
+                Console.WriteLine("Expected name of file ready for compression");
+                break;
+            }
+            default:
+            {
+                Console.WriteLine("Incorrect transformation argument");
+                break;
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Incorrect amount of arguments");
+    }
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine($"There is no such file path as: \"{input?[1]}\"");
+}
---
+++ Lzw/Lzw/TrieForLzw.cs
@@ -0,0 +1,104 @@
+﻿namespace Lzw;
+
+public class TrieForLzw
+{
+    private int prefixCount;
+    private readonly TrieElement head;
+    private TrieElement pointer;
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TrieForLzw"/> class
+    /// </summary>
+    public TrieForLzw()
+    {
+        prefixCount = 1;
+        head = new TrieElement();
+        pointer = head;
+    }
+    
+    /// <summary>
+    /// Saves node in file
+    /// </summary>
+    public void SaveTrieElement(byte value, FileStream file)
+    {
+        var writeValue = pointer.HasParent() ? pointer.Value : 0;
+        var bytes = BitConverter.GetBytes(writeValue);
+        Array.Resize(ref bytes, (int) Math.Ceiling(Math.Log2(prefixCount) / 8));
+        file.Write(bytes);
+        file.WriteByte(value);
+    }
+
+    /// <summary>
+    /// Saves node count
+    /// </summary>
+    public void SaveTrieCount(FileStream file)
+        => file.Write(BitConverter.GetBytes(prefixCount));
+
+    /// <summary>
+    /// Sets the pointer back to the head
+    /// </summary>
+    public void ResetCursor()
+        => pointer = head;
+    
+    /// <summary>
+    /// Adds child if current <see cref="TrieElement"/> doesn't have one
+    /// </summary>
+    public bool TryAdd(byte value)
+    {
+        if (pointer.HasChild(value))
+        {
+            pointer = pointer.GetChild(value);
+            return false;
+        }
+        pointer.AddChild(prefixCount, value);
+        prefixCount++;
+        return true;
+    }
+    
+    /// <summary>
+    /// Element of trie containing code and dictionary of children
+    /// </summary>
+    private class TrieElement
+    {
+        public int Value { get; set; }
+        private TrieElement? parent;
+        private readonly Dictionary<byte, TrieElement> children;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrieElement"/> class
+        /// </summary>
+        public TrieElement()
+            => children = new Dictionary<byte, TrieElement>();
+
+        /// <summary>
+        /// Adds child
+        /// </summary>
+        public void AddChild(int value, byte keyByte)
+        {
+            var newChild = new TrieElement
+            {
+                parent = this,
+                Value = value
+            };
+            children.Add(keyByte, newChild);
+        }
+
+        /// <summary>
+        /// Returns true if current <see cref="TrieElement"/> instance has a child
+        /// </summary>
+        public bool HasChild(byte keyByte)
+            => children.ContainsKey(keyByte);
+
+        /// <summary>
+        /// Returns child of current <see cref="TrieElement"/> instance
+        /// </summary>
+        public TrieElement GetChild(byte keyByte)
+            => children[keyByte];
+
+        /// <summary>
+        /// Returns true if current <see cref="TrieElement"/> instance has a parent
+        /// </summary>
+        public bool HasParent()
+            => parent != null;
+    }
+}
